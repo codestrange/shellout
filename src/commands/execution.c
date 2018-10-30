715f8acca56193b48ce839a2790e98d35ba4b444
cbermudez97 <cbermu.97@gmail.com>
2018-10-30 17:25:51 -0400

    Arreglar problemas con el método para ejecutar comandos
    Se probó el método para que funcionara correctamente con los pipes
    y las entradas y salidas por ficheros. Aún se mantiene un problema
    al cerrar los file descriptors de los ficheros y/o pipes creados
    por el método, pues parece que se quedan algunos sin cerrarse.

--- src/commands/execution.c
+++ src/commands/execution.c
@@ -21,7 +21,7 @@ int open_all_in(Command *command, int first_fd) {
         else {
             close(def_fd);
         }
-        def_fd = open(in_file,O_RDONLY | O_CREAT);
+        def_fd = open(in_file,O_RDONLY | O_CREAT, S_IRWXU | S_IRWXG | S_IROTH);
     }
     return def_fd;
 }
@@ -31,16 +31,19 @@ int open_all_out(Command *command,int first_fd) {
     for(int i = 0; i < command->len_out_files; ++i) {
         char *out_file = command->out_files[i];
         bool append = command->mask_in_files[i];
+        printf("append %d\n", append);
         if(outfirst) {
             outfirst = false;
         }
         else {
             close(def_fd);
         }
-        if(append)
-            def_fd = open(out_file, O_CREAT | O_TRUNC | O_WRONLY);
-        else
-            def_fd = open(out_file, O_CREAT | O_APPEND | O_WRONLY);
+        if(!append) {
+            def_fd = open(out_file, O_CREAT | O_TRUNC | O_WRONLY, S_IRWXU | S_IRWXG | S_IROTH);
+        }
+        else {
+            def_fd = open(out_file, O_CREAT | O_APPEND | O_WRONLY, S_IRWXU | S_IRWXG | S_IROTH);
+        }
     }
     return def_fd;
 }
@@ -48,6 +51,8 @@ int open_all_out(Command *command,int first_fd) {
 int execute_command(List commands) {
     int in = 0;
     int out = 1;
+    infirst = true;
+    outfirst = true;
     for(int i = 0; i < commands.size; ++i) {
         int pipes[2];
         pipe(pipes);
@@ -55,30 +60,32 @@ int execute_command(List commands) {
         Command actual_command = index_list(&commands, i);
         int command_input_fd = open_all_in(&actual_command, in);
         int command_output_fd = open_all_out(&actual_command, out);
-
+        // //Just for debug
+        // printf("comando -> %s\npipes[0] -> %d\npipes[1]-> %d\nin-> %d\nout-> %d\ncommand_in-> %d\ncommand_out-> %d\n",actual_command.name,pipes[0],pipes[1],in,out,command_input_fd,command_output_fd);
+        // //Just for debug
         int son = fork();
         if(son) {
-            close(pipes[2]);
+            close(pipes[1]);
             int status;
             wait(&status);
-            in = pipes[1];
+            in = pipes[0];
             char c;
-            if(i + 1 == commands.size) {
+            if(i + 1 == commands.size && command_output_fd == out) {
                 while(read(in,&c,1)) {
-                    write(stdout,&c,1);
-                    if(c == EOF)
+                    write(1,&c,1);
                 }
             }
-            else if(command_output_fd != in) {
+            else if(command_output_fd != out) {
                 while(read(in,&c,1)) {
                     write(command_output_fd,&c,1);
                 }
+                close(command_output_fd);
             }
         }
         else {
-            close(pipes[1]);
+            close(pipes[0]);
             dup2(command_input_fd, 0);
-            dup2(pipes[2], 1);
+            dup2(out, 1);
             execvp(actual_command.name, actual_command.arguments);
         }
     }
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -7,15 +7,48 @@
 List parse(char *line) {
     Command command;
     command.name = "ls";
-    command.arguments = malloc(sizeof(char*));
+    command.arguments = malloc(3 * sizeof(char*));
     command.arguments[0] = malloc(10 * sizeof(char));
-    command.arguments[0][0] = '-';
-    command.arguments[0][0] = 'l';
-    command.arguments[0][0] = 0;
+    command.arguments[0] = "ls";
     command.arguments[1] = NULL;
+    command.out_files = malloc(sizeof(char*));
+    command.out_files[0] = malloc(10 * sizeof(char));
+    command.out_files[0] = "files/a.txt";
+    command.mask_in_files = malloc(sizeof(bool*));
+    command.mask_in_files[0] = false;
     command.len_in_files = 0;
-    command.len_out_files = 0;
-    List list = new_list(1);
+    command.len_out_files = 1;
+
+    Command command1;
+    command1.name = "grep";
+    command1.arguments = malloc(3 * sizeof(char*));
+    command1.arguments[0] = malloc(10 * sizeof(char));
+    command1.arguments[0] = "grep";
+    command1.arguments[1] = malloc(10 * sizeof(char));
+    command1.arguments[1] = "i";
+    command1.arguments[2] = NULL;
+    command1.in_files = malloc(sizeof(char*));
+    command1.in_files[0] = malloc(10 * sizeof(char));
+    command1.in_files[0] = "files/a.txt";
+    command1.mask_in_files = malloc(sizeof(bool*));
+    command1.mask_in_files[0] = false;
+    command1.len_in_files = 1;
+    command1.len_out_files = 0;
+
+    Command command2;
+    command2.name = "grep";
+    command2.arguments = malloc(3 * sizeof(char*));
+    command2.arguments[0] = malloc(10 * sizeof(char));
+    command2.arguments[0] = "grep";
+    command2.arguments[1] = malloc(10 * sizeof(char));
+    command2.arguments[1] = "e";
+    command2.arguments[2] = NULL;
+    command2.len_in_files = 0;
+    command2.len_out_files = 0;
+
+    List list = new_list(10);
     append_list(&list, command);
+    append_list(&list, command1);
+    append_list(&list, command2);
     return list;
 }
