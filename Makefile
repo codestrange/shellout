4b5ac594734eaed760feb09462a672b39d6fab2d
leynier <leynier41@gmail.com>
2018-11-01 20:09:01 -0400

    Implementar la funcionalidad de history

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 run: compile
 	./bin/shellout.o
 compile:
-	gcc -o bin/shellout.o src/shellout.c src/commands/execution.c src/utils/command.c src/utils/list.c src/parser/parser.c
+	gcc -o bin/shellout.o src/shellout.c src/commands/execution.c src/utils/command.c src/utils/list.c src/parser/parser.c src/history/history.c
--- src/shellout.c
+++ src/shellout.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <signal.h>
 #include "commands/execution.h"
+#include "history/history.h"
 #include "parser/parser.h"
 #include "utils/list.h"
 #include "utils/command.h"
@@ -15,6 +16,7 @@
 extern int current_pid;
 char *current_dir;
 char *buffer;
+CharCharList history;
 
 void signals_handler(int signal) {
     if (current_pid != -1) {
@@ -25,29 +27,43 @@ void signals_handler(int signal) {
 }
 
 bool fixed_commands (char *command) {
-    if (strncmp(command, "exit", 4) == 0) {
-            exit(0);
-    }
-    if (strncmp(command, "cd", 2) == 0) {
-        CommandList commands = parse(command);
-        if (commands.size == 1) {
-            Command c = index_commandlist(&commands, 0);
-            if (c.len_arguments == 2) {
-                chdir(c.arguments[1]);
-                getcwd(current_dir, MAX_PATH);
-            } 
-            else {
-                chdir("/home");
-                getcwd(current_dir, MAX_PATH);
-            }
-            return true;
+    CommandList commands = parse(command);
+    if (!commands.size)
+        return true;
+    Command firstcommand = index_commandlist(&commands, 0);
+    if (commands.size == 1 && !strncmp(firstcommand.name, "exit", 4) && firstcommand.len_arguments == 1 && 
+        firstcommand.len_in_files == 0 && firstcommand.len_out_files == 0) {
+        set_history(&history);
+        exit(0);
+    } else if (commands.size == 1 && !strncmp(firstcommand.name, "cd", 2)) {
+        if (firstcommand.len_arguments == 2) {
+            chdir(firstcommand.arguments[1]);
+            getcwd(current_dir, MAX_PATH);
+        } else {
+            chdir("/home");
+            getcwd(current_dir, MAX_PATH);
         }
+        return true;
+    } else if (commands.size == 1 && !strncmp(firstcommand.name, "history", 7) && firstcommand.len_arguments == 1 && 
+        firstcommand.len_in_files == 0 && firstcommand.len_out_files == 0) {
+        CharList line;
+        int cont = 0;
+        for (int i = 0; i < history.size; ++i) {
+            line = index_charcharlist(&history, i);
+            printf("%d ", ++cont);
+            for (int j = 0; j < line.size; ++j) {
+                buffer = index_charlist(&line, j);
+                printf("%c", buffer);
+            }
+            printf("%c", '\n');
+        }
+        return true;
     }
     return false;
 }
 
-char * mygetline(void) {
-    char * line = malloc(100 * sizeof(char)), * linep = line;
+char *mygetline(void) {
+    char *line = malloc(100 * sizeof(char)), *linep = line;
     size_t lenmax = 100, len = lenmax;
     int c;
     while(true) {
@@ -56,7 +72,7 @@ char * mygetline(void) {
             break;
         if (--len == 0) {
             len = lenmax;
-            char * linen = realloc(linep, lenmax *= 2);
+            char *linen = realloc(linep, lenmax *= 2);
             if (linen == NULL) {
                 free(linep);
                 return NULL;
@@ -78,6 +94,17 @@ void initialize_shell() {
     getcwd(current_dir, MAX_PATH);
     buffer = malloc(MAX_PATH * sizeof(char));
     system("clear");
+    history = get_history();
+}
+
+bool same_command(char *command) {
+    CharList temp = index_charcharlist(&history, history.size - 1);
+    char *lastcommand = convert_arraychar(&temp);
+    while (*lastcommand && *command && *lastcommand == *command) {
+        command++;
+        lastcommand++;
+    }
+    return !*lastcommand && !*command;
 }
 
 int main(int argc, char **argv) {
@@ -87,6 +114,15 @@ int main(int argc, char **argv) {
     while (true) {
         printf("%s$", current_dir);
         char *line = mygetline();
+        if (!same_command(line) && *line != ' ') {
+            if (history.size == 50)
+                remove_charcharlist(&history, 0);
+            CharList charList = new_charlist(10);
+            int i = 0;
+            while (line[i])
+                append_charlist(&charList, line[i++]);
+            append_charcharlist(&history, charList);
+        }
         if (!fixed_commands(line)) {
             CommandList commands = parse(line);
             execute_command(commands);
